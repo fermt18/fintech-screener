dac25f25bac54168d37d6fc34aeea99891908f1f
Ferran Montes Torns <fmontes@servihabitat.com>
2022-05-24 11:59:44 +0200

    added dummy values for Stock instantiation

--- src/main/kotlin/com/fermt/fintech/screener/input/DataFeed.kt
+++ src/main/kotlin/com/fermt/fintech/screener/input/DataFeed.kt
@@ -14,7 +14,19 @@ class DataFeed(val tl: List<String>) {
 
     private fun fetchStock(t: String): Stock {
         return Stock(
-            ticker = t
+            ticker = t,
+            sector = "",
+            marketCap = 1L,
+            ev = 1L,
+            currentRatio = 1.0,
+            debtToEquity = 1.0,
+            roa = 1.0,
+            roe = 1.0,
+            roic = 1.0,
+            pe = 1.0,
+            evebit = 1.0,
+            evfcff = 1.0,
+            iv = 1.0
         )
     }
 }
