5a7c980da31996f4b53b44cfe2ab2dfe10f253dc
Ferran Montes Torns <fmontes@servihabitat.com>
2022-05-24 11:54:48 +0200

    stock data model

--- src/main/kotlin/com/fermt/fintech/screener/model/Stock.kt
+++ src/main/kotlin/com/fermt/fintech/screener/model/Stock.kt
@@ -1,5 +1,20 @@
 package com.fermt.fintech.screener.model
 
 data class Stock(
-    val ticker: String
+    val ticker: String,
+    val sector: String,
+    val marketCap: Long,
+    val ev: Long,
+    // Debt
+    val currentRatio: Double,
+    val debtToEquity: Double,
+    // Performance
+    val roa: Double,
+    val roe: Double,
+    val roic: Double,
+    // Valuation
+    val pe: Double,
+    val evebit: Double,
+    val evfcff: Double,
+    val iv: Double
 )
