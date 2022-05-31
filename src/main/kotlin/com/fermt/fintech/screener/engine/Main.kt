aaaf452ffafdec19c7d9c763d4f8ec1c8e6df265
Ferran Montes Torns <fmontes@servihabitat.com>
2022-05-31 09:32:26 +0200

    added main

--- src/main/kotlin/com/fermt/fintech/screener/engine/Main.kt
+++ src/main/kotlin/com/fermt/fintech/screener/engine/Main.kt
@@ -2,11 +2,17 @@ package com.fermt.fintech.screener.engine
 
 import com.fermt.fintech.screener.input.DataFeed
 import com.fermt.fintech.screener.output.CSVFile
+import java.io.File
 import java.nio.file.Paths
 
-class Main {
-
-    val df = DataFeed(Paths.get("").toUri())
+fun main() {
+    val uriPath = Paths.get("${System.getProperty("user.dir")}" +
+            "${File.separator}src" +
+            "${File.separator}main" +
+            "${File.separator}resources" +
+            "${File.separator}tickers" +
+            "${File.separator}tickerList").toUri()
+    val df = DataFeed(uriPath)
     val tickerList = df.getTickerList()
     val stockList = df.getStockList(tickerList)
     val f = CSVFile(stockList).create()
